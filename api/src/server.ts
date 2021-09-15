4e76f7a25e0488b44ddcac31317a42c1d602b1e7
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-09-15 17:29:50 +0200

    :hammer: add basic middlewares

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,20 @@
 import express, { json } from "express";
-import cookieParser from "cookie-parser";
 import { apiRouter } from "./routes/api";
+
+import helmet from "helmet";
+import csurf from "csurf";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
 
 const app = express();
 
 app.use(cookieParser());
 app.use(json());
-app.use("/api/v1", apiRouter);
+app.use(helmet());
+app.use(compression());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use("/api/v1", apiRouter, csurf({ cookie: true }));
 
 app.disable("x-powered-by");
 app.listen(parseInt(process.env["API_PORT"]!), () => console.log("ONLINE!"));
